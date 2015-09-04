d10930602f3d67569bb4b450c8f28a0e570421c5
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2015-09-04 14:52:11 +0200

    Update header license

--- arch-travis.sh
+++ arch-travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2014  Mikkel Oscar Lyderik Larsen
+# Copyright (C) 2015  Mikkel Oscar Lyderik Larsen
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
