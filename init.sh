a8e1317083c96f486de8f4da1a8112fac935df1e
bartoszek <bartoszek.github@bartus.33mail.com>
2020-08-19 15:48:13 +0200

    Fix pgp check. (#71)
    When inside volatile docker container we do not have
    `dirmngr` instantiated and keyserver has to be provided
    in `gpg` call (`gpg --keyserver ${server_url}`)
    
    Co-authored-by: bartus <szczepaniak.bartek+github@gmail.com>

--- init.sh
+++ init.sh
@@ -87,7 +87,7 @@ upgrade_system() {
 install_packages() {
   if [ ${#CONFIG_PACKAGES[@]} -gt 0 ]; then
     for pkg in "${CONFIG_PACKAGES[@]}"; do
-      yay -S "$pkg" --noconfirm --needed --useask || exit $?
+      yay -S "$pkg" --noconfirm --needed --useask --gpgflags "--keyserver hkp://pool.sks-keyservers.net" || exit $?
     done
   fi
 }
